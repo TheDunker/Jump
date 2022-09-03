05d8703c9a1c1fe491a5f6cd0e1ea40366a515ff
TheDuker <ridermatt13@gmail.com>
2022-09-03 14:39:37 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,25 +14,24 @@ function Plane(props) {
 }
 
 function Cube(props) {
-  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+  const [ref] = useBox(() => ({ mass: 1, position: [0, 4, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+  
   return (
     <mesh receiveShadow castShadow ref={ref}>
       <boxGeometry />
-      <meshLambertMaterial color="hotpink" />
+      <meshLambertMaterial color="red" />
     </mesh>
   )
 }
 
 ReactDOM.render(
-  <Canvas shadows dpr={[1, 2]} gl={{ alpha: false }} camera={{ position: [-1, 5, 5], fov: 45 }}>
+  <Canvas>
     <color attach="background" args={['lightblue']} />
     <ambientLight />
     <directionalLight position={[10, 10, 10]} castShadow shadow-mapSize={[2048, 2048]} />
     <Physics>
-      <Plane position={[0, -2.5, 0]} />
+      <Plane position={[0, 0, 0]} />
       <Cube position={[0.1, 5, 0]} />
-      <Cube position={[0, 10, -1]} />
-      <Cube position={[0, 20, -2]} />
     </Physics>
   </Canvas>,
   document.getElementById('root')
